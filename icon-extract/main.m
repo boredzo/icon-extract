15a8d20bb3d9f9c967d7c2f1aa22cc14b07c4f44
Peter Hosey <prh@Featherweight.surfcity-hb.org>
2011-10-29 09:51:01 -0700

    Initial Commit

---
+++ icon-extract/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  icon-extract
+//
+//  Created by Peter Hosey on 2011-10-29.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
